669b0f6ef43f99e16d604fb8f471b0cb1c2c5f0c
BigBangTheoryy <akshaypathak001@gmail.com>
2026-07-26 07:40:45 +0000

    working on Follow/Unfollow feature for users

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth  import views as auth_views
+    from django.contrib.auth  import views as auth_views
 from django.urls import path, include
 from . import views
 
@@ -24,6 +24,7 @@ urlpatterns =  [
     path("register/", views.register, name = "register"),
     path("edit/", views.edit, name = "edit"),
     path("users/", views.user_list, name="user_list"),
+    path("users/follow/", views.user_follow, name = "user_follow"),
     path("users/<str:username>", views.user_detail, name="user_detail"),
 
     ]
--- account/views.py
+++ account/views.py
@@ -5,7 +5,7 @@ from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditFor
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
-
+from django.views.decorators.http import require_POST
 
 
 User = get_user_model()
@@ -118,6 +118,30 @@ def user_detail(request, username):
 
 
 
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action  = request.POST.get("action")
+
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+
+            if action == "Follow":
+                Contact.objects.get_or_create(user_from = request.user, user_to = user)
+
+            else:
+                Contact.objects.filter(user_from = request.user, user_to = user).delete()
+
+
+            return JsonResponse({"status": "ok"})
+
+        except User.DoesNotExist:
+            return JsonResponse({"status":"error"})
+
+
+    return JsonResponse({"status":"error"})
 
 
 
